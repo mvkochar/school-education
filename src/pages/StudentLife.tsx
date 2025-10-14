ac07d2a056d8549f94eef006d37e8cd53bb4c18e
sgkochar <sgkochar@gmail.com>
2025-10-14 13:49:10 +0300

    add Activities, Features and Support blocks to StudentLife page

--- src/pages/StudentLife.tsx
+++ src/pages/StudentLife.tsx
@@ -9,15 +9,198 @@ const StudentLife = () => {
                     <h1 className="page-bl-title">Embracing Learning with Discovery and Joy</h1>
                 </div>
                 <p className="page-bl-desc">
-                    Welcome to our Student Life page, where the magic of 
-                    childhood comes alive through the enriching experiences 
-                    and adventures that our students embark on each day. 
-                    At our kindergarten school, we believe that learning goes 
-                    beyond textbooks, and we strive to create a holistic and 
-                    engaging environment that nurtures every aspect of a child's 
+                    Welcome to our Student Life page, where the magic of
+                    childhood comes alive through the enriching experiences
+                    and adventures that our students embark on each day.
+                    At our kindergarten school, we believe that learning goes
+                    beyond textbooks, and we strive to create a holistic and
+                    engaging environment that nurtures every aspect of a child's
                     development.
                 </p>
             </main>
+            <section className="student-activities">
+                <p className="page-bl-above">Our Features</p>
+                <h2 className="page-bl-title">Extracurricular Activities</h2>
+                <p className="page-bl-desc">
+                    At Little Learners Academy, we believe in nurturing well-rounded individuals.
+                    Our extracurricular activities offer a diverse range of experiences that complement
+                    our academic curriculum and encourage students to explore their interests and passions.
+                    We offer a wide array of extracurricular activities, including
+                </p>
+                <div className="student-activities-box d-f">
+                    <div className="student-activities-item">
+                        <div className="activities-item-icon"><img src="/images/activities/1.png" alt="activities1" /></div>
+                        <div className="activities-item-content">
+                            <h3 className="activities-item-title">Sports and Athletics</h3>
+                            <p className="activities-item-desc">
+                                Students can participate in various sports, from soccer
+                                and basketball to gymnastics and yoga. Sports help promote
+                                teamwork, physical fitness, and a sense of discipline.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="student-activities-item">
+                        <div className="activities-item-icon"><img src="/images/activities/2.png" alt="activities2" /></div>
+                        <div className="activities-item-content">
+                            <h3 className="activities-item-title">Art and Creativity</h3>
+                            <p className="activities-item-desc">
+                                Our art classes and creative workshops provide a platform
+                                for students to express their creativity through painting,
+                                drawing, and other artistic forms.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="student-activities-item">
+                        <div className="activities-item-icon"><img src="/images/activities/3.png" alt="activities3" /></div>
+                        <div className="activities-item-content">
+                            <h3 className="activities-item-title">Music and Performing Arts</h3>
+                            <p className="activities-item-desc">
+                                Students can discover their musical talents through singing,
+                                playing musical instruments, and participating in drama and
+                                theater performances.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="student-activities-item">
+                        <div className="activities-item-icon"><img src="/images/activities/4.png" alt="activities4" /></div>
+                        <div className="activities-item-content">
+                            <h3 className="activities-item-title">Language Clubs</h3>
+                            <p className="activities-item-desc">
+                                Language clubs offer an opportunity for students to immerse
+                                themselves in different languages and cultures, fostering global awareness.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="student-activities-item">
+                        <div className="activities-item-icon"><img src="/images/activities/5.png" alt="activities5" /></div>
+                        <div className="activities-item-content">
+                            <h3 className="activities-item-title">Science Club</h3>
+                            <p className="activities-item-desc">
+                                The science club allows young scientists to explore
+                                the wonders of science through fun experiments and
+                                hands-on learning.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="student-activities-item">
+                        <div className="activities-item-icon"><img src="/images/activities/6.png" alt="activities6" /></div>
+                        <div className="activities-item-content">
+                            <h3 className="activities-item-title">Cooking and Culinary Arts</h3>
+                            <p className="activities-item-desc">
+                                Cooking classes introduce students to the joys
+                                of preparing and tasting delicious and healthy meals.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className='student-features'>
+                <p className="page-bl-above">Our Features</p>
+                <h2 className="page-bl-title">Events & Celebrations</h2>
+                <p className="page-bl-desc">
+                    At Little Learners Academy, we celebrate every milestone
+                    and create cherished memories for our students. Throughout
+                    the year, we host a variety of events and celebrations that
+                    bring the entire school community together. Some of our memorable
+                    events include
+                </p>
+                <div className="student-features-box d-f">
+                    <div className="student-features-item">
+                        <div><img src="/images/student-features/1.png" alt="student-features1" /></div>
+                        <h3 className="features-item-title">Annual Sports Day</h3>
+                        <p className="features-item-desc">
+                            A day filled with friendly competition, team spirit, and sportsmanship.
+                        </p>
+                    </div>
+                    <div className="student-features-item">
+                        <div><img src="/images/student-features/2.png" alt="student-features2" /></div>
+                        <h3 className="features-item-title">Cultural Festivals</h3>
+                        <p className="features-item-desc">
+                            Celebrations of diverse cultural festivals,
+                            promoting cultural exchange and appreciation.
+                        </p>
+                    </div>
+                    <div className="student-features-item">
+                        <div><img src="/images/student-features/3.png" alt="student-features3" /></div>
+                        <h3 className="features-item-title">Art Exhibitions</h3>
+                        <p className="features-item-desc">
+                            Showcasing our students' artistic talents through exhibitions and displays.
+                        </p>
+                    </div>
+                    <div className="student-features-item">
+                        <div><img src="/images/student-features/4.png" alt="student-features4" /></div>
+                        <h3 className="features-item-title">Science Fair</h3>
+                        <p className="features-item-desc">
+                            A platform for budding scientists to present
+                            their innovative projects and experiments.
+                        </p>
+                    </div>
+                    <div className="student-features-item">
+                        <div><img src="/images/student-features/5.png" alt="student-features5" /></div>
+                        <h3 className="features-item-title">International Day</h3>
+                        <p className="features-item-desc">
+                            A vibrant celebration of our diverse community,
+                            embracing cultures from around the world.
+                        </p>
+                    </div>
+                    <div className="student-features-item">
+                        <div><img src="/images/student-features/6.png" alt="student-features6" /></div>
+                        <h3 className="features-item-title">Graduation Ceremony</h3>
+                        <p className="features-item-desc">
+                            A significant milestone as our Kindergarten students
+                            prepare to embark on their academic journey.
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <section className='student-support'>
+                <p className="page-bl-above">Our Achievements</p>
+                <h2 className="page-bl-title">Student Support</h2>
+                <p className="page-bl-desc">
+                    At Little Learners Academy, we are committed to providing
+                    a supportive and nurturing environment that meets the unique
+                    needs of each student. Our student support services include
+                </p>
+                <div className="student-support-box d-f">
+                    <div className="student-support-item">
+                        <div className="support-item-icon">
+                            <img src="/images/student-support/1.png" alt="student-support1" />
+                        </div>
+                        <div className="support-item-content">
+                            <h3 className="support-item-title">Counseling</h3>
+                            <p className="support-item-desc">
+                                Professional counselors offer guidance and support
+                                to students, addressing their emotional and social well-being.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="student-support-item">
+                        <div className="support-item-icon">
+                            <img src="/images/student-support/2.png" alt="student-support2" />
+                        </div>
+                        <div className="support-item-content">
+                            <h3 className="support-item-title">Learning Support</h3>
+                            <p className="support-item-desc">
+                                Our educators provide additional assistance to students
+                                who may require extra support in their academic journey.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="student-support-item">
+                        <div className="support-item-icon">
+                            <img src="/images/student-support/3.png" alt="student-support3" />
+                        </div>
+                        <div className="support-item-content">
+                            <h3 className="support-item-title">Parent-Teacher Collaboration</h3>
+                            <p className="support-item-desc">
+                                We foster a strong partnership with parents 
+                                to ensure seamless communication and mutual 
+                                support in a child's development.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
